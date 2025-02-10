a17e6c1028664516459b39f3702eaf51cce06d3d
NivakaranS <nivakaran@hotmail.com>
2025-02-10 11:39:37 +0530

    first commit

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,12 +1,15 @@
 
 import React from 'react';
+import Logo from '../images/icon2.png'
+import Image from 'next/image';
 
 const Loading = () => {
   return (
     <div className="flex items-center w-screen text-white justify-center h-screen">
       <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
-        <span className="visually-hidden">Loading...</span>
+        <Image src={Logo} height={30}/>
       </div>
+      <span className="visually-hidden">Loading...</span>
     </div>
   );
 };
