bd3e68314620ce77e086204851c8d87f8e786427
NivakaranS <nivakaran@hotmail.com>
2025-02-10 11:51:02 +0530

    first commit

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -4,12 +4,20 @@ import Logo from '../images/codebugfinal8.png'
 import Image from 'next/image';
 
 const Loading = () => {
+    const [dots, setDots] = useState("");
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+        setDots((prev) => (prev.length < 3 ? prev + "." : ""));
+        }, 500);
+        return () => clearInterval(interval);
+    }, []);
+
+
   return (
     <div className="flex space-y-3 flex-col items-center w-screen text-white justify-center h-screen">
-        <Image src={Logo} height={100}/>
-      <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
-        
-      </div>
+        <Image src={Logo} className="mb-2" height={100}/>
+        <p>Loading{dots}</p>
       
     </div>
   );
