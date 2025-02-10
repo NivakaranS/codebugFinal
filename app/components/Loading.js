8a3534c14ac881eac064b6d2dee71bf8b30f7c82
NivakaranS <nivakaran@hotmail.com>
2025-02-10 11:41:22 +0530

    first commit

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -6,10 +6,11 @@ import Image from 'next/image';
 const Loading = () => {
   return (
     <div className="flex items-center w-screen text-white justify-center h-screen">
-      <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
         <Image src={Logo} height={30}/>
+      <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
+        
       </div>
-      <span className="visually-hidden">Loading...</span>
+      
     </div>
   );
 };
